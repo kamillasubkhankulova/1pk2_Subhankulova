06c133d9681d33913681cc2bbe6f090bf5ced002
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-18 20:36:18 +0500

    выполнено задание 4 урока 2

---
+++ Task4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Numerics;
+
+namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nВведите свой год рождения: \n");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите свой месяц рождения: \n");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите дату рождения: \n");
+            int day = int.Parse(Console.ReadLine());
+            var today = DateTime.Today;
+            DateTime birthday = new DateTime(year, month, day);
+            int age = today.Year - birthday.Year;
+            {
+                if (birthday > today.AddYears(-age)) age--;
+                if (age < 18)
+                    Console.WriteLine("\nПользователь не совершеннолетнй: \n");
+                else
+                    Console.WriteLine("\nПользователь совершеннолетний\n");
+            }
+
+        }
+    }
+}
