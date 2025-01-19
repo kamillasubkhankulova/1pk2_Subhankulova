15b2331ff412c3e0519ae54060bd956b5d69d722
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-19 17:35:22 +0500

    решено задание 4 урока 3

---
+++ Task_04_03/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_04_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string user;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите текст (введите 'exit' или оставьте пустую строку для выхода):");
+                user = Console.ReadLine();
+
+                if (user == "exit" || string.IsNullOrEmpty(user))
+                {
+                    break;
+                }
+
+                count++;
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Количество введенных строк: {count}");
+        }
+    }
+}
