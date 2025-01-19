447b7fcc83112217310fab0eda44500e230ad9d1
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-19 18:34:04 +0500

    решено задание 6 урока 3

---
+++ Task_06_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_06_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("| X  |  Y |");
+            Console.WriteLine("-------------------------");
+            for (double x = -5; x <= 4; x += 0.5)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"| {x,6:F2} | {y,6:F2} |");
+            }
+            Console.WriteLine("------------------");
+        }
+    }
+}
