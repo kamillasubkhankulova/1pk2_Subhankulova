2ea355e81f1062c08e4ae7b24e9b38916b14ae33
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-20 18:58:06 +0500

    выполнено задание 8 урока 3

--- Task_08_03/Program.cs
+++ Task_08_03/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Task_08_03
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace Task_08_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int n = 20; n <= 50; n++)
+            {
+                if (n % 3 == 0 & n % 5 != 0)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+
+
         }
     }
 }
