b4ab2ef0ac79853bdaed8c03628296f85fe1ea82
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-20 19:44:09 +0500

    выполнено задание 9 урока 3

---
+++ Task_03_09/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_03_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальную сумму вклада: ");
+            if (!double.TryParse(Console.ReadLine(), out double x))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+
+            Console.Write("Введите процент годовых: ");
+            if (!double.TryParse(Console.ReadLine(), out double p))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+
+            Console.Write("Введите конечную сумму вклада: ");
+            if (!double.TryParse(Console.ReadLine(), out double y))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+            if (x <= 0 || p <= 0 || y <= x)
+            {
+                Console.WriteLine("Некорректные входные данные");
+                return;
+            }
+
+
+            int years = 0;
+            while (x < y)
+            {
+                x = Math.Floor(x * (1 + p / 100));
+                years++;
+            }
+
+            Console.WriteLine($"Через {years} лет вклад составит не менее {y} рублей.");
+        }
+    }
+}
