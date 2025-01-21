1c3be95bfd9bb438b4e8e195eca30f55a52f6b0f
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-21 17:42:43 +0500

    выполнено задание 4, урока 2

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double g = 9.8;
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("| Время    | Скорость      |");
+            Console.WriteLine("------------------");
+
+            for (double t = 0; t <= 15; t += 0.5)
+            {
+                double v = g * t;
+                Console.WriteLine($"| {t,8:F1} | {v,14}");
+            }
+        }
+    }
+}
