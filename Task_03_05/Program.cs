5facfd04dd9c5cc9b0974d9d2437103442fb24b6
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-23 18:58:34 +0500

    выполнено задание 5, урока 3.

--- Task_03_05/Program.cs
+++ Task_03_05/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //Написать программу, которая выводит на экран таблицу соответствия температуры в градусах Цельсия и Фаренгейта(F = C * 1, 8 + 32).Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во время работы программы
+
             Console.Write("Введите начальную температуру в градусах Цельсия: ");
-            if (!double.TryParse(Console.ReadLine(), out double startCelsius))
+            if (!double.TryParse(Console.ReadLine(), out double startCelsius)) //вводим переменную начальные цельсия и проверяем вводимые данные пользователем на корректность
             {
                 Console.WriteLine("Некорректный ввод.");
                 return;
@@ -24,23 +26,23 @@
                 Console.WriteLine("Некорректный ввод.");
                 return;
             }
-            if (step <= 0)
+            if (step <= 0) //проверяем на наличие ошибок вбиваемых данных
             {
                 Console.WriteLine("Шаг должен быть положительным числом.");
                 return;
             }
 
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("-------------------------"); //создаем таблицу, чтобы позже вносить в нее данные
             Console.WriteLine("| Цельсий | Фаренгейт |");
             Console.WriteLine("-------------------------");
 
             for (double celsius = startCelsius; celsius <= endCelsius; celsius += step)
             {
-                double fahrenheit = celsius * 1.8 + 32;
-                Console.WriteLine($"| {celsius,7:F2} | {fahrenheit,10:F2} |");
+                double fahrenheit = celsius * 1.8 + 32; // вычисляем значение в фаренгейтах
+                Console.WriteLine($"| {celsius,7:F2} | {fahrenheit,10:F2} |"); //вносим данные в таблицу
             }
 
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("-------------------------"); //закрываем таблицу
         }
     }
 }
