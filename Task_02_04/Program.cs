05688f6c780bb9bf2adbd0c3c30b29cd7440a381
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-23 18:47:13 +0500

    выполнено задание 4, урока 2.

--- Task_02_04/Program.cs
+++ Task_02_04/Program.cs
@@ -7,17 +7,19 @@ namespace Task_02_02
     {
         static void Main(string[] args)
         {
+            //Пользователь вводит свою дату рождения построчно (сначала год, потом месяц и в конце дату) произведите расчет является ли пользователь совершеннолетним на текущую дату и выведите соответствующее сообщение об этом.
+            
             Console.WriteLine("\nВведите свой год рождения: \n");
             int year = int.Parse(Console.ReadLine());
             Console.WriteLine("\nВведите свой месяц рождения: \n");
             int month = int.Parse(Console.ReadLine());
             Console.WriteLine("\nВведите дату рождения: \n");
-            int day = int.Parse(Console.ReadLine());
-            var today = DateTime.Today;
+            int day = int.Parse(Console.ReadLine()); //ввожу три переменных, которые будут отвечать за дату рождения пользователя.
+            var today = DateTime.Today; //ввожу переменную today, которая будет отвечать за настоящую дату
             DateTime birthday = new DateTime(year, month, day);
-            int age = today.Year - birthday.Year;
+            int age = today.Year - birthday.Year; 
             {
-                if (birthday > today.AddYears(-age)) age--;
+                if (birthday > today.AddYears(-age)) age--; //если дата рождения больше чем нынешняя дата, то вычитается один год
                 if (age < 18)
                     Console.WriteLine("\nПользователь не совершеннолетнй: \n");
                 else
