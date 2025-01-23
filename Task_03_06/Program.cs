99430abf52591b9cff847adcab59cf2d7164c052
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-23 19:01:39 +0500

    выполнено задание 6, урока 3.

--- Task_03_06/Program.cs
+++ Task_03_06/Program.cs
@@ -6,13 +6,14 @@ namespace Task_06_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-------------------------");
+            //Написать программу, которая выводит таблицу значений функции: у= |х| для -45x≤4, с шагом h = 0,5.
+            Console.WriteLine("-------------------------"); //создаем таблицу
             Console.WriteLine("| X  |  Y |");
             Console.WriteLine("-------------------------");
-            for (double x = -5; x <= 4; x += 0.5)
+            for (double x = -5; x <= 4; x += 0.5) //проверяем условия
             {
                 double y = Math.Abs(x);
-                Console.WriteLine($"| {x,6:F2} | {y,6:F2} |");
+                Console.WriteLine($"| {x,6:F2} | {y,6:F2} |"); //вносим данные 
             }
             Console.WriteLine("------------------");
         }
