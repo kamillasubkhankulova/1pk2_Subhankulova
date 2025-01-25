147bceac823d8ac0570384c0a79fce4e184cd9f2
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-25 21:36:20 +0500

    выполнено задание 8, урока 4

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дан массив, содержащий последовательность 50 случайных чисел. Найти количество пар элементов, значения которых равны.
+            int[] number = new int[50];
+            Random rnd = new Random();
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = rnd.Next(1, 101);
+                Console.Write(" "+number[i]+" ");
+            }
+            int couple = 0;
+            for (int i = 0;i < number.Length; i++)
+            {
+                for(int j = 0; j<i;  j++) if( number[i] == number[j]) couple++;
+             
+            }
+            Console.WriteLine($"Количество пар - {couple}");
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
