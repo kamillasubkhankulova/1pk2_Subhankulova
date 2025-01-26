c1f4187fb44fe8433d3d075b0020c3415c7dc1c0
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-26 17:03:53 +0500

    выполнено задание 4, урока 5

---
+++ Task_05_04/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+
+namespace Task_05_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дан квадратный массив размерность n*n. Произведите анализ данной матрицы и выясните является ли данная матрица диагональной(все элементы вне главной диагонали равны нулю)Если матрица является диагональной, то вывести ее повторно с цветовым выделением главной диагонали. Если нет, то вывеси сообщение что матрица не является диагональной.
+            int n = 3;
+            int[,] mass = new int[n, n];
+            Random rnd = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i, j] = (char)(rnd.Next(0, 10));
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            bool dio = true;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j && mass[i, j] != 0)
+                    {
+                        dio = false;
+                    }
+                }
+            }
+
+            if (dio)
+            {
+                Console.WriteLine("Матрица является диагональной. Вывод с выделением главной диагонали:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan; 
+                            Console.Write(mass[i, j] + " ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White; 
+                            Console.Write(mass[i, j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Матрица не является диагональной.");
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
