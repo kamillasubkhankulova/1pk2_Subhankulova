a29520821fa4e52a5a4d37dc5a04f5131659e770
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-08 19:38:39 +0500

    Выполнено задание 2, урока 11

---
+++ Task_11_02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_11_02
+{
+    internal class Program
+    //Передача по ссылке (ref): Напишите метод, который принимает два целых числа по ссылке и меняет их местами.Проверьте, изменились ли значения переменных вне метода
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine($"До вызова метода: x={x}, y={y}");
+            Revers(ref x, ref y);
+            Console.WriteLine($"После вызова метода: x={x}, y={y}");
+        }
+        /// <summary>
+        /// переворачивает значения, которые были заданы
+        /// </summary>
+        /// <param переменная="a"></param>
+        /// <param переменная="b"></param>
+        public static void Revers(ref int a, ref int b)
+        {
+            int n = a;
+            a = b;
+            b = n;
+            Console.WriteLine($"Внутри метода: {a}, {b}");
+        }
+        
+    }
+}
