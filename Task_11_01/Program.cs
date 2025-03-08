1f3fcf7eabbc71f139467df95249799d480887a1
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-08 19:28:28 +0500

    Выполнено задание 1, урока 11

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            int[] mass = {x, y};
+            Console.WriteLine($"До вызова метода: x={x}, y={y}");
+            ReversNum(mass);
+            Console.WriteLine($"После вызова метода: x={x}, y={y} ");
+            Console.WriteLine($"После вызова метода {mass[0]}, {mass[1]}");
+        }
+        /// <summary>
+        /// переворачивает значения массива
+        /// </summary>
+        /// <param name="mass"></param>
+        public static void ReversNum(int[] mass)
+        {
+            int n = mass[0];
+            mass[0] = mass[1];
+            mass[1] = n;
+            Console.WriteLine($"Внутри метода: {mass[0]}, {mass[1]}");
+        }
+    }
+}
