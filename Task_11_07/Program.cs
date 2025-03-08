ebd1129696a37cb5a985b9e56c1958d0f56bfba5
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-08 21:20:43 +0500

    Выполнено задание 7, урока 11

---
+++ Task_11_07/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_11_07
+{
+    internal class Program
+    //Передача массива по ссылке (ref): Напишите метод, который принимает массив целых чисел по ссылке и изменяет его элементы, увеличивая каждый на 1. Проверьте, изменился ли оригинальный массив вне метода.
+    {
+        static void Main(string[] args)
+        {
+            int[] Mass = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("До вызова метода: ");
+            PrintArray(Mass);
+            IncrementArrayElements(ref Mass);
+            Console.WriteLine("После вызова метода: ");
+            PrintArray(Mass);
+        }
+        /// <summary>
+        /// увеличивает каждый элемент в массие на 1 единицу
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void IncrementArrayElements(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]++;
+            }
+            Console.WriteLine("Внутри метода: ");
+            PrintArray(arr);
+        }
+        /// <summary>
+        /// Выводит массив
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("[");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                if (i < arr.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
