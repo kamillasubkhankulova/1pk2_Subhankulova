0dfd88e43d46c0bf449d0bf8eaf828a26b50eef9
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-08 20:35:57 +0500

    Выполнено задание 4, урока 11

---
+++ Task_11_04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_11_04
+{
+    internal class Program
+    {
+        //Массив параметров (params): Напишите метод, который принимает массив чисел и возвращает их среднее значение.Используйте ключевое слово params
+        static void Main(string[] args)
+        {
+            double average1 = TheAverageNumber(10, 20, 30, 40, 50);
+            Console.WriteLine($"Среднее значение: {average1}");
+        }
+        /// <summary>
+        /// Находит среднее значение чисел в массиве
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static double TheAverageNumber(params double[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+            }
+            return sum / numbers.Length;
+        }
+        
+    }
+}
