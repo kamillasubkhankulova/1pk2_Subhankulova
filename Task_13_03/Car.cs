7b61b30ea67c233362d002e6fe5ff0389fe24825
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-19 19:43:19 +0500

    Выполнено задание 3, урока 13

---
+++ Task_13_03/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_03
+{
+    internal class Car
+    {
+        #region Свойства
+        public string Number { get; set; }
+        public string Brand { get; set; }
+        public string Colors { get; set; }
+        public double Speed { get; set; }
+        #endregion
+
+        #region Конструктор
+        public Car(string number, string brand, string colors, double speed)
+        {
+            Number = number;
+            Brand = brand;
+            Colors = colors;
+            Speed = speed;
+        }
+        #endregion
+        #region Метод
+        public void boostCar(double acceleration)
+        {
+            Speed += acceleration;
+        }
+        public void Brake(int maxSpeed)
+        {
+            if (Speed > maxSpeed)
+            {
+                Speed = 0;
+                Console.WriteLine($"Автомобиль остановился, превышена допустимая скорость! Текущая скорость: {Speed} км/ч.");
+            }
+            else
+            {
+                Console.WriteLine($"Автомобиль {Brand} тормозит. Текущая скорость: {Speed} км/ч.");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Task_13_03/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_13_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car ("A123BC", "Toyota", "Blue", 50 );
+            myCar.boostCar(50);
+            myCar.boostCar(30);
+            myCar.Brake(70);
+            myCar.Brake(100);
+        }
+    }
+}
