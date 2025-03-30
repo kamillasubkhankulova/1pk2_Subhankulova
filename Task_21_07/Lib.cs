975ed318e6a642849b7e4bb7d7493c60c8f026ec
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-30 18:15:44 +0500

    выполнено задание 7, урока 21

---
+++ Task_21_07/Lib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_07
+{
+    internal class Lib
+    {
+        public static string FindFirstKey(Dictionary<string, int> dictionary, int value)
+        {
+            foreach (string key in dictionary.Keys)
+            {
+                int nowValue = dictionary[key];
+                if (nowValue == value)
+                {
+                    return key;
+                }
+            }
+            return null;
+        }
+    }
+}
+
---
+++ Task_21_07/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_21_07
+{
+    internal class Program
+    {
+        //Написать метод, который находит первый ключ в словаре, соответствующий заданному значению. Еслизначениянет, вернуть null.
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> newDiction = new Dictionary<string, int>()
+        {
+            {"math", 1},
+            {"PI", 2},
+            {"Russian", 3},
+            {"physics", 2}
+        };
+            string keyFor2 = Lib.FindFirstKey(newDiction, 2);
+            Console.WriteLine($"Ключ для значения 2: {keyFor2}");
+            string keyFor5 = Lib.FindFirstKey(newDiction, 5);
+            Console.WriteLine($"Ключ для значения 5: {keyFor5}");
+        }
+    }
+}
