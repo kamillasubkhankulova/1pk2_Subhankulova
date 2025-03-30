27c61ef4532ae69fc43500ca1263b69ac655db86
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-30 18:02:02 +0500

    выполнено задание 6, урока 21

---
+++ Task_21_06/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_06
+{
+    internal class List
+    {
+        public static List<int> Remove(List<int> list)
+        {
+            List<int> elements = new List<int>();
+            SortedSet<int> seen = new SortedSet<int>();
+            foreach (int el in list)
+            {
+                if (!seen.Contains(el))
+                {
+                    elements.Add(el);
+                    seen.Add(el);
+                }
+            }
+            return elements;
+        }
+    }
+}
+
---
+++ Task_21_06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    {
+        //Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок.
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 4, 5, 1 };
+            List<int> uniqueNumbers = List.Remove(numbers);
+            Console.WriteLine("Исходный список:");
+            PrintList(numbers);
+            Console.WriteLine("Список без дубликатов:");
+            PrintList(uniqueNumbers);
+            static void PrintList(List<int> list)
+            {
+                foreach (int item in list)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
