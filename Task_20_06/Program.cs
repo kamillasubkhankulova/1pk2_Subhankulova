047ac3ae06e84cc9e08a79b1bc5ac23058fb872a
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-30 17:46:21 +0500

    выполнено задание 6, урока 20

---
+++ Task_20_06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_20_06
+{
+    internal class Program
+    {
+        /*Создайте программу, имитирующую работу светофора, используя перечисление TrafficLightColor:
+       • Red
+       • Yellow
+       • Green
+        Реализуйте автоматическое переключение цветов (каждые 3 секунды).
+        При смене цвета выводите его в консоль (можно с задержкой Thread.Sleep).
+        Добавьте возможность ручного переключения (например, по нажатию клавиши).*/
+        static void Main(string[] args)
+        {
+            TrafficActions trafficLight = new TrafficActions();
+            Thread auto = new Thread(trafficLight.RunAutomatic);
+            auto.Start();
+            Console.WriteLine("Светофор запущен в автоматическом режиме. " + "Нажмите любую клавишу для ручного переключения.");
+            while (true)
+            {
+                trafficLight.ManualChange();
+            }
+        }
+    }
+}
---
+++ Task_20_06/TrafficActions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_06
+{
+    /*Реализуйте автоматическое переключение цветов (каждые 3 секунды).
+     При смене цвета выводите его в консоль (можно с задержкой Thread.Sleep).
+     Добавьте возможность ручного переключения (например, по нажатию клавиши).*/
+    internal class TrafficActions
+    {
+        private TrafficLightColor nowColor;
+        public TrafficActions()
+        {
+            nowColor = TrafficLightColor.Red;
+        }
+        private void PrintColor()
+        {
+            Console.WriteLine($"Текущий цвет: {nowColor}");
+        }
+        // Метод для переключения цветов
+        public void RunAutomatic()
+        {
+            while (true)
+            {
+                PrintColor();
+                Thread.Sleep(3000);
+                switch (nowColor)
+                {
+                    case TrafficLightColor.Red:
+                        nowColor = TrafficLightColor.Yellow;
+                        break;
+                    case TrafficLightColor.Yellow:
+                        nowColor = TrafficLightColor.Green;
+                        break;
+                    case TrafficLightColor.Green:
+                        nowColor = TrafficLightColor.Red;
+                        break;
+                }
+            }
+        }
+        //ручное изменение цвета
+        public void ManualChange()
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            switch (nowColor)
+            {
+                case TrafficLightColor.Red:
+                    nowColor = TrafficLightColor.Yellow;
+                    break;
+                case TrafficLightColor.Yellow:
+                    nowColor = TrafficLightColor.Green;
+                    break;
+                case TrafficLightColor.Green:
+                    nowColor = TrafficLightColor.Red;
+                    break;
+            }
+            PrintColor();
+        }
+    }
+}
