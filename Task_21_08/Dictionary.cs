2802116e179ed9fa7aff71a0a49887e11d1be107
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-30 18:36:58 +0500

    выполнено задание 8, урока 21

---
+++ Task_21_08/Dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_08
+{
+    internal class Dictionary
+    {
+        public static string[] GetStrings(params string[] inputStrings)
+        {
+            SortedSet<string> uniqueStrings = new SortedSet<string>();
+            foreach (string str in inputStrings)
+            {
+                uniqueStrings.Add(str);
+            }
+            return uniqueStrings.ToArray();
+        }
+    }
+}
---
+++ Task_21_08/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        //напишите метод, который на вход получается массив параметров (строк) и возвращает только уникальные строки
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "red", "green", "blue", "red", "green" };
+            string[] unique3 = Dictionary.GetStrings(inputArray);
+            PrintStrings(unique3);
+            static void PrintStrings(string[] strings)
+            {
+                foreach (string str in strings)
+                {
+                    Console.Write(str + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
