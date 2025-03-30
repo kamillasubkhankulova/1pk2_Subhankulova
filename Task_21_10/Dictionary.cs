45765bcf0ac3030130ac1f3b92159274a6d21cd9
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-03-30 19:29:39 +0500

    выполнено задание 10, урока 21

---
+++ Task_21_10/Dictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_10
+{
+    //Написать метод, который объединяет два словаря. Если ключ присутствует в обоих словарях, суммировать их значения.
+    internal class Dictionary
+    {
+        public static Dictionary<string, int> MergeDictionaries(Dictionary<string, int> dict1, Dictionary<string, int> dict2)
+        {
+            Dictionary<string, int> mergedDictionary = new Dictionary<string, int>(dict1);
+            foreach (var el in dict2)
+            {
+                string key = el.Key;
+                int value = el.Value;
+                if (mergedDictionary.ContainsKey(key))
+                {
+                    mergedDictionary[key] += value;
+                }
+                else
+                {
+                    mergedDictionary.Add(key, value);
+                }
+            }
+            return mergedDictionary;
+        }
+    }
+
+}
+
---
+++ Task_21_10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_21_10
+{
+    internal class Program
+    {
+        //Написать метод, который объединяет два словаря. Если ключ присутствует в обоих словарях, суммировать их значения.
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictA = new Dictionary<string, int>()
+        {
+            {"IT", 1},
+            {"PI", 2},
+            {"MATH", 3}
+        };
+
+            Dictionary<string, int> dictB = new Dictionary<string, int>()
+        {
+            {"IT", 4},
+            {"BIOLOGY", 5},
+            {"ALGEBRA", 6}
+        };
+
+            Dictionary<string, int> merged = Dictionary.MergeDictionaries(dictA, dictB);
+
+
+            Console.WriteLine("Объединенный словарь:");
+            PrintDictionary(merged);
+        }
+        static void PrintDictionary(Dictionary<string, int> dict)
+        {
+            foreach (var el in dict)
+            {
+                Console.Write($"{el.Key}: {el.Value}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
