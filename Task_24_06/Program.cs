d64b4aeea7f751d52c37efdcc904b6e0223c8d74
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-04-04 18:17:23 +0500

    выполнено задание 6, урока 24

---
+++ Task_24_06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_24_06
+{
+    internal class Program
+    {
+        //Напишите метод, который принимает путь к файлу и возвращает количество строк в нем. ИспользуйтеStreamReader
+        static void Main(string[] args)
+        {
+            string Path = @"C:\Proga\ProgaFile.txt";
+            int lines = CountLines(Path);
+            if (lines != -1)
+            {
+                Console.WriteLine($"Количество строк: {lines}");
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Подсчет количества строк в файле
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <returns></returns>
+        public static int CountLines(string Path)
+        {
+            int lineCount = 0;
+            if (File.Exists(Path))
+            {
+                using (StreamReader reader = new StreamReader(Path))
+                {
+                    while (reader.ReadLine() != null)
+                    {
+                        lineCount++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Файл {Path} не найден.");
+                return -1;
+            }
+            return lineCount;
+        }
+    }
+}
