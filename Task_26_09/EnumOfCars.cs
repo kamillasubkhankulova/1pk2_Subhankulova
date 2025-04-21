21c72d64f6435fdb471309a15babbc090ca134cd
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-04-21 20:47:40 +0500

    Практика

---
+++ Task_26_09/EnumOfCars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+
+namespace Task_26_09
+{
+    //Создать перечисления категории автомобиля (экном класс, комфорт класс, бизнес класс, премиум класс)
+    enum CarType
+    {
+        Economy=1,
+        Comfort,
+        Business,
+        Premium
+    }
+}
